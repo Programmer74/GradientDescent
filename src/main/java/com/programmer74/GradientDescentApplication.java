554206d866f4230d2a388b584e1e75719bf12110
Nikita Tomilov <programmer174@icloud.com>
2018-04-25 23:53:33 +0300

    GradientDescentApplication: better cli
    PLEASE NOTE THAT THIS HAS HARDCODED JAR PATH
    WITHOUT ASSEMBLED JAR WITH OUR CUSTOM CLASSES WHICH WE GIVE TO SPARK
    THE APP ON THE SPARK CLUSTER WILL DIE WITH AN ERROR OF INABILITY TO CAST
    CLASS
    I WILL GET RID OF THIS HARDCODE SOMEHOW LATER ON

--- src/main/java/com/programmer74/GradientDescentApplication.java
+++ src/main/java/com/programmer74/GradientDescentApplication.java
@@ -37,38 +37,98 @@ public class GradientDescentApplication {
         return avgTime;
     }
 
+    public static void usage() {
+        System.out.println("Usage:\n$ java -jar GradientDescent.jar (--no-spark | <master url>) <csv filename>");
+    }
 
     public static void main(String[] args) {
-        //List<Pair<Double>> data = DummyDataLoader.loadBigDummyData(new LinearHypothesis(), samplesCount, 0.001);
-        List<Pair<Double>> data = DummyDataLoader.importCSV("test.csv");
-        System.out.println("Input dataset size: " + data.size());
-        GradientDescentCalculator calculator = new BasicGradientDescentCalculator(data, new LinearHypothesis());
 
-        System.out.println("Benchmarking Basic GradientDescent");
-        double avgTime = benchmark(calculator);
-        System.out.println("Basic Gradient Descent required " + avgTime + "ms to calculate.");
+        boolean withoutSpark = false;
+        boolean localMode = false;
+        int localThreadCount = 0;
+        String masterLocation = "";
+        String fileName = "";
 
-        System.out.println("Firing up Spark...");
+        if (args.length < 2) {
+            usage();
+            return;
+        }
 
-        Logger.getLogger("org").setLevel(Level.WARN);
-        Logger.getLogger("akka").setLevel(Level.WARN);
+        if (args[0].equals("--no-spark")) {
+            withoutSpark = true;
+            System.out.println("Set mode to NO_SPARK");
+        } else {
+            if (args[0].matches("^local[\\[][0-9]+[]]$")) {
+                System.out.println("Set mode to SPARK_LOCAL_CLUSTER");
+                localMode = true;
+                localThreadCount = Integer.valueOf(args[0].replaceAll("[^0-9]", ""));
+                System.out.println("Local mode with thread count : " + localThreadCount);
+            } else if (args[0].matches("^spark://.+")) {
+                System.out.println("Set mode to SPARK_CLUSTER at " + args[0]);
+                localMode = false;
+                masterLocation = args[0];
+            } else {
+                System.out.println(args[0]);
+                usage();
+                return;
+            }
+        }
 
-        SparkConf conf = new SparkConf().setAppName("GradientDescentApplication")
-                .setMaster("local[2]")
-                .set("spark.driver.host", "localhost");
-        JavaSparkContext sc = new JavaSparkContext(conf);
+        fileName = args[1];
 
-        List<Integer> dummydata = Arrays.asList(1, 2, 3, 4, 5);
-        JavaRDD<Integer> distData = sc.parallelize(dummydata);
-        int sum = distData.reduce((a, b) -> (a + b));
+        GradientDescentCalculator calculator = null;
 
-        System.out.println("Spark says that the sum of " + dummydata + " is " + sum);
+        if (withoutSpark) {
+            System.out.println("Importing file " + fileName);
 
-        //calculator = new SparkGradientDescentCalculator(sc, sc.parallelize(data), new LinearHypothesis());
-        calculator = new SparkGradientDescentCalculator(sc, DummyDataLoader.importCSV(sc, "test.csv"), new LinearHypothesis());
+            long startTime = System.currentTimeMillis();
+            List<Pair<Double>> data = DummyDataLoader.importCSV(fileName);
+            long endTime   = System.currentTimeMillis();
+            long totalTime = endTime - startTime;
 
-        System.out.println("Benchmarking Spark GradientDescent");
-        avgTime = benchmark(calculator);
-        System.out.println("Spark Gradient Descent required " + avgTime + "ms to calculate.");
+            System.out.println("File import time: " + totalTime + " ms");
+            System.out.println("Dataset size: " + data.size());
+
+            calculator = new BasicGradientDescentCalculator(data, new LinearHypothesis());
+        } else {
+            System.out.println("Configuring Spark...");
+            Logger.getLogger("org").setLevel(Level.WARN);
+            Logger.getLogger("akka").setLevel(Level.WARN);
+
+            SparkConf conf = new SparkConf().setAppName("GradientDescentApplication");
+
+            if (localMode) {
+                conf.setMaster("local[" + localThreadCount + "]")
+                    .set("spark.driver.host", "localhost");
+            } else {
+                conf.setMaster(masterLocation);
+                conf.setJars(new String[]{"/home/hotaro/IdeaProjects/GradientDescent/target/GradientDescent-0.1-SNAPSHOT.jar"});
+            }
+
+            JavaSparkContext sc = new JavaSparkContext(conf);
+
+            System.out.println("Importing file " + fileName);
+
+            long startTime = System.currentTimeMillis();
+            JavaRDD<Pair<Double>> data = DummyDataLoader.importCSV(sc, fileName);
+            long endTime   = System.currentTimeMillis();
+            long totalTime = endTime - startTime;
+
+            System.out.println("File import time: " + totalTime + " ms");
+            System.out.println("Dataset size: " + data.count());
+
+            calculator = new SparkGradientDescentCalculator(sc, data, new LinearHypothesis());
+        }
+
+        long startTime = System.currentTimeMillis();
+
+        Pair<Double> finalTheta = calculator.calculate(0.1, 0.1);
+
+        System.out.printf("theta0 = %f, theta1 = %f\n", finalTheta.getFirst(), finalTheta.getSecond());
+        System.out.printf("y = %f * x + %f\n", finalTheta.getSecond(), finalTheta.getFirst());
+        long endTime   = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        System.out.println("Took " + totalTime + " ms to calculate.");
+
     }
 }
